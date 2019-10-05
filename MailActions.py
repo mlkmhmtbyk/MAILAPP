59867b5cffb1ea5075c0a19c13bf9bcb989dd632
mlkmhmtbyk <biyikm16@itu.edu.tr>
2019-10-05 14:18:23 +0300

    added google api

--- MailActions.py
+++ MailActions.py
@@ -1,19 +1,48 @@
-import os
-import smtplib
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
-EMAIL_ADDRESS = 'compproj1itu@gmail.com'
-EMAIL_PASSWORD = 'Compproj1010'
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.ehlo()
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
 
-    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    service = build('gmail', 'v1', credentials=creds)
 
-    subject = 'Grab dinner this weekend?'
-    body = 'How about dinner at 6pm this saturday'
+    # Call the Gmail API
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
 
-    msg = f'Subject: {subject}\n\n {body}'
+    if not labels:
+        print('No labels found.')
+    else:
+        print('Labels:')
+        for label in labels:
+            print(label['name'])
 
-    smtp.sendmail(EMAIL_ADDRESS,'mlkmhmtbyk@gmail.com', msg)
+if __name__ == '__main__':
+    main()
